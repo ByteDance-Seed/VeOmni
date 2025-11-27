e7c3d3b834c6941f09a6efff4362694193e5c015
feng0w0 <houyufeng4@huawei.com>
2025-11-27 15:46:20 +0800

    [helper]:fix npu profiling

--- veomni/utils/helper.py
+++ veomni/utils/helper.py
@@ -620,8 +620,7 @@ def create_profiler(
     def handler_fn(p):
         time = int(datetime.datetime.now().timestamp())
 
-        # torch_npu does not support export gzip trace json directly
-        trace_file_extention = "pt.trace.json" if IS_NPU_AVAILABLE else "pt.trace.json.gz"
+        trace_file_extention = "pt.trace.json.gz"
         gpu_memory_file_extension = "pkl"
 
         if trace_dir.startswith("hdfs://"):
@@ -634,23 +633,17 @@ def create_profiler(
             trace_file = os.path.join(trace_dir, f"veomni_rank{global_rank}_{time}.{trace_file_extention}")
             gpu_memory_file = os.path.join(trace_dir, f"veomni_rank{global_rank}_{time}.{gpu_memory_file_extension}")
 
-        p.export_chrome_trace(trace_file)
+        if IS_NPU_AVAILABLE:
+            nonlocal npu_trace_handler
+            npu_trace_handler(p)
+            trace_file = p.prof_if.prof_path
+        elif IS_CUDA_AVAILABLE:
+            p.export_chrome_trace(trace_file)
         logger.info(f"Profiling result saved at {trace_file}.")
 
         if IS_CUDA_AVAILABLE or IS_NPU_AVAILABLE:
             get_torch_device().memory._dump_snapshot(gpu_memory_file)
             logger.info(f"Profiling memory visualization saved at {gpu_memory_file}.")
-
-        # In NPU, compress the trace file to .gz format ourselves.
-        if IS_NPU_AVAILABLE:
-            gz_path = trace_file + ".gz"
-            import gzip
-
-            with open(trace_file, "rb") as f_in, gzip.open(gz_path, "wb") as f_out:
-                f_out.write(f_in.read())
-            os.remove(trace_file)
-            trace_file = gz_path
-            logger.info(f"Profiling result compressed to {trace_file}.")
 
         if trace_dir.startswith("hdfs://"):
             copy(trace_file, trace_dir)
@@ -667,11 +660,10 @@ def create_profiler(
     if IS_NPU_AVAILABLE:
         profiler_module = torch_npu.profiler
         activities = [profiler_module.ProfilerActivity.CPU, profiler_module.ProfilerActivity.NPU]
-        trace_handler_fn = torch_npu.profiler.tensorboard_trace_handler(trace_dir)
+        npu_trace_handler = torch_npu.profiler.tensorboard_trace_handler(CACHE_DIR if trace_dir.startswith("hdfs://") else trace_dir)
     else:
         profiler_module = torch.profiler
         activities = [profiler_module.ProfilerActivity.CPU, profiler_module.ProfilerActivity.CUDA]
-        trace_handler_fn = handler_fn
 
     warmup = 0 if start_step == 1 else 1
     wait = start_step - warmup - 1
@@ -687,7 +679,7 @@ def create_profiler(
     base_profiler = profiler_module.profile(
         activities=activities,
         schedule=schedule,
-        on_trace_ready=trace_handler_fn,
+        on_trace_ready=handler_fn(),
         record_shapes=record_shapes,
         profile_memory=profile_memory,
         with_modules=True,
