6c7975beaa9702e5ef78cd5805da7c611168053a
Crystal-jiang <934889998@qq.com>
2025-11-27 19:47:46 +0800

    fix train get NPROC_PER_NODE on npu

--- train.sh
+++ train.sh
@@ -6,7 +6,13 @@ export TOKENIZERS_PARALLELISM=false
 export TORCH_NCCL_AVOID_RECORD_STREAMS=1
 
 NNODES=${NNODES:=1}
-NPROC_PER_NODE=${NPROC_PER_NODE:=$(nvidia-smi --list-gpus | wc -l)}
+if command -v nvidia-smi &> /dev/null && nvidia-smi --list-gpus &> /dev/null; then
+  # GPU
+  NPROC_PER_NODE=${NPROC_PER_NODE:=$(nvidia-smi --list-gpus | wc -l)}
+else
+  # NPU
+  NPROC_PER_NODE=${NPROC_PER_NODE:=$(ll /dev/davinci* | grep -v "davinci_manager" | wc -l)}
+fi
 NODE_RANK=${NODE_RANK:=0}
 MASTER_ADDR=${MASTER_ADDR:=0.0.0.0}
 MASTER_PORT=${MASTER_PORT:=12345}
