4e8760ec6e60df4cd86101d8a3048dd074dfd952
Luosuu <zhluosuu@outlook.com>
2025-12-04 06:17:52 +0000

    simplify code path

--- veomni/distributed/clip_grad_norm.py
+++ veomni/distributed/clip_grad_norm.py
@@ -1,7 +1,5 @@
-from ..utils.import_utils import is_torch_npu_available
 from .fsdp import clip_grad_norm_ as fsdp1_clip_grad_norm
 from .fsdp2 import clip_grad_norm as fsdp2_clip_grad_norm
-from .fsdp2 import npu_fsdp2_clip_grad_norm
 from .parallel_state import get_parallel_state
 
 
@@ -12,13 +10,8 @@ def veomni_clip_grad_norm(
     dp_mode = parallel_state.dp_mode
     if dp_mode == "fsdp1":
         grad_norm = fsdp1_clip_grad_norm(model, max_norm, norm_type)
-    elif dp_mode == "fsdp2" and not is_torch_npu_available():
-        # fsdp2 on GPU
+    elif dp_mode == "fsdp2":
         grad_norm = fsdp2_clip_grad_norm(model, max_norm, norm_type, error_if_nonfinite, foreach)
-    elif dp_mode == "fsdp2" and is_torch_npu_available():
-        # fsdp2 on NPU, where we have to manually reduce gradients
-        # context: https://github.com/ByteDance-Seed/VeOmni/issues/241
-        grad_norm = npu_fsdp2_clip_grad_norm(model, max_norm, norm_type, error_if_nonfinite, foreach)
     else:
         raise RuntimeError(f"Unknown dp mode {dp_mode}")
 
--- veomni/distributed/fsdp2/clip_grad_norm.py
+++ veomni/distributed/fsdp2/clip_grad_norm.py
@@ -66,7 +66,14 @@ def ep_fsdp2_clip_grad_norm(
     non_ep_params: List[torch.nn.Parameter] = [
         p for p in model._ep_param_groups.get("non_ep", []) if p.grad is not None
     ]
-
+    device_type = get_device_type()
+    if device_type == "npu" and ps.ep_enabled and ps.ep_fsdp_size > 1 and ep_params:
+        # Averaging gradients for EP params through dividing grads by ep_fsdp_size
+        # this is to simulate fsdp2 set_gradient_divide_factor
+        scale = 1.0 / float(ps.ep_fsdp_size)
+        for q in ep_params:
+            if q.grad is not None:
+                q.grad.detach().mul_(scale)
     # Compute and reduce non-EP
     logger.debug_rank0(f"non_ep_params reduce group is {fsdp_group=}")
     non_ep_total = _fsdp2_reduce_group(
