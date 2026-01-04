51b4b8ccceb191e2fddd29b68636d40b268fa20b
phdddd <penghengduo@huawei.com>
2026-01-04 11:44:22 +0800

    code format check

--- veomni/models/transformers/qwen3_vl/npu_patch.py
+++ veomni/models/transformers/qwen3_vl/npu_patch.py
@@ -15,9 +15,7 @@
 # limitations under the License.
 
 from . import modeling_qwen3_vl
-
 from ....ops.npu_patch import npu_fused_operator
-
 
 def apply_qwen3vl_npu_patch():
     # Patches for Qwen3VL Model
