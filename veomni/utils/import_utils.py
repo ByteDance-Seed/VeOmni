ce5045f2917e9319c5768182771c7f13b7b7ca93
wang-hua-2019 <62917673+wang-hua-2019@users.noreply.github.com>
2026-01-26 23:47:18 +0800

    add linux aarch64 platform check

--- veomni/utils/import_utils.py
+++ veomni/utils/import_utils.py
@@ -92,6 +92,11 @@ def is_veomni_patch_available() -> bool:
     return _PACKAGE_FLAGS["veomni_patch"]
 
 
+def is_linux_arrch64_platform() -> bool:
+    import platform
+    return platform.machine().lower() == "aarch64"
+
+
 _FFMPEG_AVAILABLE = None
 
 
