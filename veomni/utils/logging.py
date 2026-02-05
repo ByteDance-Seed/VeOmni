f84395b2d84773f6d716401e37645270f981ae39
shizhelun <shizhelun@bytedance.com>
2026-02-05 17:33:19 +0800

    fix logging once

--- veomni/utils/logging.py
+++ veomni/utils/logging.py
@@ -21,33 +21,13 @@ import os
 import sys
 import threading
 from functools import lru_cache
+from logging import Logger as _Logger
 from typing import Optional
 
 
 _thread_lock = threading.RLock()
 _default_handler: Optional["logging.Handler"] = None
 _default_log_level: "logging._Level" = logging.INFO
-
-
-class _Logger(logging.Logger):
-    """
-    A logger that supports info_rank0.
-    """
-
-    def info_rank0(self, msg: str) -> None:
-        self.info(msg)
-
-    def info_once(self, msg: str) -> None:
-        self.info_once(msg)
-
-    def warning_rank0(self, msg: str) -> None:
-        self.warning(msg)
-
-    def warning_once(self, msg: str) -> None:
-        self.warning_once(msg)
-
-    def debug_rank0(self, msg: str) -> None:
-        self.debug(msg)
 
 
 def _get_default_logging_level() -> "logging._Level":
@@ -67,7 +47,7 @@ def _get_library_name() -> str:
     return __name__.split(".")[0]
 
 
-def _get_library_root_logger() -> "logging.Logger":
+def _get_library_root_logger() -> "_Logger":
     return logging.getLogger(_get_library_name())
 
 
@@ -101,6 +81,9 @@ def get_logger(name: Optional[str] = None) -> "_Logger":
         name = _get_library_name()
 
     _configure_library_root_logger()
+    _Logger.info_once = info_once
+    _Logger.warning_once = warning_once
+    _Logger.debug_once = debug_once
     return logging.getLogger(name)
 
 
@@ -112,41 +95,38 @@ def set_verbosity_info() -> None:
     _get_library_root_logger().setLevel(logging.INFO)
 
 
-def info_rank0(self: "logging.Logger", *args, **kwargs) -> None:
+def info_rank0(self: "_Logger", *args, **kwargs) -> None:
     if int(os.getenv("LOCAL_RANK", "0")) == 0:
         kwargs["stacklevel"] = kwargs.get("stacklevel", 1) + 1
         self.info(*args, **kwargs)
 
 
-logging.Logger.info_rank0 = info_rank0
+_Logger.info_rank0 = info_rank0
 
 
-def debug_rank0(self: "logging.Logger", *args, **kwargs) -> None:
+def debug_rank0(self: "_Logger", *args, **kwargs) -> None:
     if int(os.getenv("LOCAL_RANK", "0")) == 0:
         kwargs["stacklevel"] = kwargs.get("stacklevel", 1) + 1
         self.debug(*args, **kwargs)
 
 
-logging.Logger.debug_rank0 = debug_rank0
+_Logger.debug_rank0 = debug_rank0
 
 
-def warning_rank0(self: "logging.Logger", *args, **kwargs) -> None:
+def warning_rank0(self: "_Logger", *args, **kwargs) -> None:
     if int(os.getenv("LOCAL_RANK", "0")) == 0:
         kwargs["stacklevel"] = kwargs.get("stacklevel", 1) + 1
         self.warning(*args, **kwargs)
 
 
-logging.Logger.warning_rank0 = warning_rank0
+_Logger.warning_rank0 = warning_rank0
 
 
 @lru_cache(None)
-def info_once(self: "logging.Logger", *args, **kwargs) -> None:
+def info_once(self: "_Logger", *args, **kwargs) -> None:
     if int(os.getenv("LOCAL_RANK", "0")) == 0:
         kwargs["stacklevel"] = kwargs.get("stacklevel", 1) + 1
         self.info_rank0(*args, **kwargs)
-
-
-logging.Logger.info_once = info_once
 
 
 @lru_cache(None)
@@ -156,4 +136,8 @@ def warning_once(self, *args, **kwargs) -> None:
         self.warning_rank0(*args, **kwargs)
 
 
-logging.Logger.warning_once = warning_once
+@lru_cache(None)
+def debug_once(self: "_Logger", *args, **kwargs) -> None:
+    if int(os.getenv("LOCAL_RANK", "0")) == 0:
+        kwargs["stacklevel"] = kwargs.get("stacklevel", 1) + 1
+        self.debug_rank0(*args, **kwargs)
