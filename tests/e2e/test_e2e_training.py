bfea6d4b482279c7a27dda88b743d1341cc4d682
shizhelun <shizhelun@bytedance.com>
2026-02-09 15:00:22 +0800

    fix

--- tests/e2e/test_e2e_training.py
+++ tests/e2e/test_e2e_training.py
@@ -14,7 +14,7 @@ test_cases = [
 @pytest.mark.parametrize("task_name", test_cases)
 def test_e2e_training(task_name):
     exec_script = E2E_TEST_SCRIPT[task_name]
-    e2e_test_res = subprocess.run(exec_script, shell=True, check=True)
+    e2e_test_res = subprocess.run(exec_script, shell=True, check=True, capture_output=True, text=True)
     print(e2e_test_res.stdout)
     exec_log_df = parse_training_log(e2e_test_res.stdout)
     print(exec_log_df)
