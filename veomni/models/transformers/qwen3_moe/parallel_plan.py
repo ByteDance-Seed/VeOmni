ad3845d8543458850f47701e88c7f80b14d882a2
yifan.pi <yifan.pi@bytedance.com>
2026-02-21 01:56:37 +0000

    proper parallel plan

--- veomni/models/transformers/qwen3_moe/parallel_plan.py
+++ veomni/models/transformers/qwen3_moe/parallel_plan.py
@@ -1,14 +1,24 @@
 from torch.distributed._tensor import Shard
 
 from ....distributed.parallel_plan import ParallelPlan
+from ....utils.import_utils import is_transformers_version_greater_or_equal_to
 
 
 def get_parallel_plan():
-    ep_plan = {
-        "model.layers.*.mlp.experts.gate_proj": Shard(0),
-        "model.layers.*.mlp.experts.up_proj": Shard(0),
-        "model.layers.*.mlp.experts.down_proj": Shard(0),
-    }
+    if is_transformers_version_greater_or_equal_to("5.0.0"):
+        # Since transformers v5, HuggingFace changed the per-expert keys to merged expert tensors and concatenated
+        # gate and up-projection. In transformers 4.x, veomni asks for merged expert tensors and do not concatenate
+        # gate and up-projection.
+        ep_plan = {
+            "model.layers.*.mlp.experts.gate_up_proj": Shard(0),
+            "model.layers.*.mlp.experts.down_proj": Shard(0),
+        }
+    else:
+        ep_plan = {
+            "model.layers.*.mlp.experts.gate_proj": Shard(0),
+            "model.layers.*.mlp.experts.up_proj": Shard(0),
+            "model.layers.*.mlp.experts.down_proj": Shard(0),
+        }
     parallel_plan = ParallelPlan(
         ep_plan=ep_plan,
     )
