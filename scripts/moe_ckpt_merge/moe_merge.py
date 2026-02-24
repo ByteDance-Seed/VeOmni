4d1b528d7833c30e705ee5ccfe1e72f0a8f3f429
ting.yang <ting.yang@bytedance.com>
2026-02-24 08:40:41 +0000

    Ting: add try catch block

--- scripts/moe_ckpt_merge/moe_merge.py
+++ scripts/moe_ckpt_merge/moe_merge.py
@@ -128,7 +128,10 @@ def main(raw_hf_path, merge_hf_path):
 
     config = AutoConfig.from_pretrained(raw_hf_path)
     tokenizer = build_tokenizer(raw_hf_path)
-    processor = build_processor(raw_hf_path)
+    try:
+        processor = build_processor(raw_hf_path)
+    except Exception:
+        processor = None
 
     safetensor_files = list(glob(os.path.join(raw_hf_path, "*.safetensors")))
     safetensor_files.sort()
@@ -145,7 +148,7 @@ def main(raw_hf_path, merge_hf_path):
     for group in moe_groups:
         _merge_experts_for_group(new_state_dict, group)
 
-    model_assets = [config, tokenizer, processor]
+    model_assets = [asset for asset in [config, tokenizer, processor] if asset is not None]
     save_model_weights(merge_hf_path, new_state_dict, model_assets=model_assets)
 
 
