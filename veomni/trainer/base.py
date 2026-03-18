81d6d70f66659fc7259d8905a2ebcbbfd7a12222
Coach257 <45021066+Coach257@users.noreply.github.com>
2026-03-18 12:11:28 +0800

    [data] fix: use dataloader args (#571)

--- veomni/trainer/base.py
+++ veomni/trainer/base.py
@@ -289,8 +289,10 @@ class BaseTrainer(Stateful, ABC):
 
     def _build_dataloader(self):
         args: VeOmniArguments = self.args
+        dataloader_kwargs = asdict(args.data.dataloader)
+        dataloader_type = dataloader_kwargs.pop("type")
         self.train_dataloader = build_dataloader(
-            dataloader_type=args.data.dataloader.type,
+            dataloader_type=dataloader_type,
             dataset=self.train_dataset,
             micro_batch_size=args.train.micro_batch_size,
             global_batch_size=args.train.global_batch_size,
@@ -301,12 +303,9 @@ class BaseTrainer(Stateful, ABC):
             bsz_warmup_init_mbtoken=args.train.bsz_warmup_init_mbtoken,
             dyn_bsz=args.train.dyn_bsz,
             dyn_bsz_buffer_size=args.data.dyn_bsz_buffer_size,
-            num_workers=args.data.dataloader.num_workers,
-            drop_last=args.data.dataloader.drop_last,
-            pin_memory=args.data.dataloader.pin_memory,
-            prefetch_factor=args.data.dataloader.prefetch_factor,
             seed=args.train.seed,
             collate_fn=self.collate_fn,
+            **dataloader_kwargs,
         )
 
     def _build_parallelized_model(self):
