094a19d1d0619fbbc7662e5c7e7073cc789a4bec
Lu Di <deerlu@users.noreply.github.com>
2026-03-26 11:52:45 +0800

    [model] feat: [transformers v5] support qwen35 moe vit (#602)

--- veomni/data/dataset.py
+++ veomni/data/dataset.py
@@ -100,7 +100,8 @@ class InterleavedIterableDataset(IterativeDataset):
         for sample in self._data:
             if self._transform is not None:
                 ds_idx = sample["ds_idx"]
-                transformed_sample = self._transform(sample)
+                source_name = sample.get("source_name", None)
+                transformed_sample = self._transform(sample, source_name=source_name)
                 if isinstance(transformed_sample, List):
                     for idx in range(len(transformed_sample)):
                         transformed_sample[idx]["ds_idx"] = ds_idx
@@ -124,7 +125,8 @@ class InterleavedMappingDataset(MappingDataset):
         if self._transform is not None:
             sample = self._data[mapped_idx]
             ds_idx = sample["ds_idx"]
-            transformed_sample = self._transform(sample)
+            source_name = sample.get("source_name", None)
+            transformed_sample = self._transform(sample, source_name=source_name)
             if isinstance(transformed_sample, List):
                 for idx in range(len(transformed_sample)):
                     transformed_sample[idx]["ds_idx"] = ds_idx
