cfddc5ec2a225d342999deb353d7998c2c06aa97
wanjuncheng.123 <wanjuncheng.123@bytedance.com>
2026-03-30 14:23:09 +0800

    realize in parallel_load_safetensors

--- veomni/distributed/torch_parallelize.py
+++ veomni/distributed/torch_parallelize.py
@@ -181,7 +181,7 @@ def parallelize_model_fsdp1(
         shard_states = kwargs.get("shard_states", {})
         if weights_path:
             shard_states = parallel_load_safetensors(
-                weights_path, cpu_load_param_name=kwargs.get("cpu_load_param_name", None)
+                weights_path, cpu_load_param_name=getattr(model.get_parallel_plan(), "cpu_load_param_name", None)
             )
 
         fsdp_kwargs["param_init_fn"] = parallel_init_fsdp_fn(
