27b87237d4ad8e621af8a15f6fbb2a34909c1f5f
UserChen666 <57744890+UserChen666@users.noreply.github.com>
2026-04-18 16:30:55 +0800

    [task] fix: fix bug for args.train.accelerator.fsdp_config.mixed_precision.enable (#638)
    Co-authored-by: FoolPlayer <45593998+FoolPlayer@users.noreply.github.com>

--- tasks/deprecated_task/train_wan.py
+++ tasks/deprecated_task/train_wan.py
@@ -197,7 +197,7 @@ def main():
         weights_path=args.model.model_path,
         enable_full_shard=args.train.accelerator.fsdp_config.full_shard,
         enable_reshard_after_forward=args.train.accelerator.fsdp_config.reshard_after_forward,
-        enable_mixed_precision=args.train.enable_mixed_precision,
+        mixed_precision=args.train.accelerator.fsdp_config.mixed_precision,
         enable_gradient_checkpointing=args.train.gradient_checkpointing.enable,
         init_device=args.train.init_device,
         enable_fsdp_offload=args.train.accelerator.fsdp_config.offload,
@@ -366,7 +366,7 @@ def main():
                     noise,
                     timestep,
                     args.train.micro_batch_size,
-                    args.train.enable_mixed_precision,
+                    args.train.accelerator.fsdp_config.mixed_precision.enable,
                 )
                 training_target = flow_scheduler.training_target(latents, noise, timestep)
                 # predict noise
