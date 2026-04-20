dca9336433451aa6f2e25f852a5d74e146ce3331
Ting <ting.yang@bytedance.com>
2026-04-20 11:20:41 -0700

    [model] feat: guard unmigrated models against transformers v5 (#675)

--- veomni/models/transformers/qwen2_5_omni/__init__.py
+++ veomni/models/transformers/qwen2_5_omni/__init__.py
@@ -11,11 +11,13 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from ...loader import MODEL_CONFIG_REGISTRY, MODEL_PROCESSOR_REGISTRY, MODELING_REGISTRY
+from ...loader import MODEL_CONFIG_REGISTRY, MODEL_PROCESSOR_REGISTRY, MODELING_REGISTRY, raise_if_not_migrated_to_v5
 
 
 @MODEL_CONFIG_REGISTRY.register("qwen2_5_omni")
 def register_qwen2_5_omni_config():
+    raise_if_not_migrated_to_v5("qwen2_5_omni")
+
     from .configuration_qwen2_5_omni import Qwen2_5OmniConfig, apply_veomni_qwen25_omni_patch
 
     apply_veomni_qwen25_omni_patch()
@@ -25,6 +27,8 @@ def register_qwen2_5_omni_config():
 
 @MODELING_REGISTRY.register("qwen2_5_omni")
 def register_qwen2_5_omni_modeling(architecture: str):
+    raise_if_not_migrated_to_v5("qwen2_5_omni")
+
     from .modeling_qwen2_5_omni import (
         Qwen2_5OmniForConditionalGeneration,
         Qwen2_5OmniThinkerForConditionalGeneration,
@@ -41,6 +45,8 @@ def register_qwen2_5_omni_modeling(architecture: str):
 
 @MODEL_PROCESSOR_REGISTRY.register("Qwen2_5OmniProcessor")
 def register_qwen2_5_omni_processor():
+    raise_if_not_migrated_to_v5("qwen2_5_omni")
+
     from .processing_qwen2_5_omni import Qwen2_5OmniProcessor, apply_veomni_qwen25_omni_patch
 
     apply_veomni_qwen25_omni_patch()
